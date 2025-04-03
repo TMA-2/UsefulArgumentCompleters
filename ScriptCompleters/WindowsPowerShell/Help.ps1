1b06463620e4044aceb264ed518e3d89e7749798
Jon D. <tma2@live.com>
2025-04-03 17:10:56 -0500

    Added Update-Help & Save-Help -Module completion for Windows PowerShell

---
+++ ScriptCompleters/WindowsPowerShell/Help.ps1
@@ -0,0 +1,18 @@
+using module .\Classes\CompletionHelper.psm1
+
+Register-ArgumentCompleter -CommandName Update-Help,Save-Help -ParameterName Module -ScriptBlock {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
+
+    $WildcardInput = [CompletionHelper]::TrimQuotes($wordToComplete) + '*'
+    foreach ($Module in [CompletionHelper]::GetCachedResults('Get-Module -ListAvailable | Where-Object -Property RepositorySourceLocation -NE $null | Select-Object -ExpandProperty Name | Sort-Object -Unique', $false))
+    {
+        if ($null -eq $Module)
+        {
+            continue
+        }
+        if ($Module -like $WildcardInput)
+        {
+            [CompletionHelper]::NewParamCompletionResult($Module)
+        }
+    }
+}
